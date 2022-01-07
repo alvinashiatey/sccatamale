ffceffa4a0a30d1d53d50d7bd81092a8d3f439f6
Alvin Ashiatey <alvinashiatey@gmail.com>
2022-01-06 20:44:54 -0500

    update to order of programs

--- src/utils/dateStructure.js
+++ src/utils/dateStructure.js
@@ -1,11 +1,11 @@
 function dateStructure(date, position) {
   // split date string "Thu, 14 Jan 2021 00:00:00 GMT" to return "Thu, 14 Jan" if position is "start" and return "Thu, 14 Jan 2021" if position is "end"
   if (position === "start") {
-    return date.split(" ").slice(0, 3).join(" ");
+    return String(date).split(" ").slice(0, 3).join(" ");
   } else if (position === "end") {
-    return date.split(" ").slice(0, 4).join(" ");
+    return String(date).split(" ").slice(0, 4).join(" ");
   }
-  return date.split(" ").slice(0, 4).join(" ");
+  return String(date).split(" ").slice(0, 4).join(" ");
 }
 
 module.exports = dateStructure;
